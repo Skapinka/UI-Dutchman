d7190a986f4a62dda7c5dee1456d09e14b3c8afc
Skapinka <viktorjohansson@live.se>
2022-02-22 14:16:42 +0100

    Working on menu loading

--- js/Dictionary.js
+++ js/Dictionary.js
@@ -4,21 +4,36 @@
 var language = 'en'
 
 dict = {
-    'keys' : ['title', 'langtext', 'loginTitle'], // string keys
+    'keys' : ['title', 'langtext', 'menu_all', 'menu_beer', 'menu_wine', 'menu_spirits',
+                'menu_ecologic', 'menu_kosher', 'menu_specials'], // string keys
     'pics' : ['flag'],           // picture keys
 
     // English dictionary
     'en': {
         'title': "The Flying Dutchman",
         'flag': "img/en.png",
-        'loginTitle': "Log in"
+        "menu_all": "All",
+        "menu_beer": "Beer",
+        "menu_wine": "Wine",
+        "menu_spirits": "Spirits",
+        "menu_ecologic": "Eco",
+        "menu_kosher": "Kosher",
+        "menu_specials": "Specials"
+
     },
 
     // Swedish dictionary
     'sv': {
         'title': "Den Flygande Holländaren",
         'flag': "img/sv.png",
-        'loginTitle': "Logga in"
+        "menu_all": "Alla",
+        "menu_beer": "Öl",
+        "menu_wine": "Vin",
+        "menu_spirits": "Sprit",
+        "menu_ecologic": "Eko",
+        "menu_kosher": "Kosher",
+        "menu_specials": "Specialare"
+
     },
 }
 
