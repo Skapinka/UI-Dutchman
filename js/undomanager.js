52f5b1e4eaa784f1999d621d0d86ad3d9cf7da7c
Skapinka <viktorjohansson@live.se>
2022-02-24 21:23:10 +0100

    fixes

---
+++ js/undomanager.js
@@ -0,0 +1,31 @@
+/** Undo-redo stuff */
+
+var undoStack = new Array(); // Fill this with past actions
+var redoStack = new Array(); // Fill this with "future" actions
+
+// This undo manager is functionally identical to Lars' example code.
+//
+
+// doFunc is used whenever a 'new' function is executed (as opposed to
+// redone from the redo-stack). Resets the redo-stack.
+function doFunc(func) {
+    func.execute();
+    undoStack.push(func);
+    redoStack = [];
+    update_view();
+}
+
+// Undo function
+function undoFunc() {
+    funcobj = undoStack.pop();
+    funcobj.unexecute();
+    redoStack.push.apply(funcobj);
+}
+
+// Redo function
+
+function redoFunc() {
+    funcobj = redoStack.pop();
+    funcobj.reexecute();
+    undoStack.push(funcobj);
+}
